d3696648df8772632359b0ec174c788048da4dc1
Richard Beneyton <richard.beneyton@gmail.com>
2021-12-06 13:51:11 +0100

    day 2, part 2

--- src/main.rs
+++ src/main.rs
@@ -84,7 +84,19 @@ fn solve(day: Day, part: u8, input: String) -> String {
 
             format!("{}", horizontal * depth)
         } else {
-            String::from("")
+            let (horizontal, depth, _aim) = input.lines()
+                .fold((0, 0, 0), |(hor, depth, aim), line| {
+                    let (mut inc_hor, mut inc_depth, mut inc_aim) = (0, 0, 0);
+                    let words : Vec<_> = line.split_whitespace().collect();
+                    if words.len() == 2 {
+                        let step = words[1].parse::<i64>().unwrap();
+                        if words[0] == "forward" { inc_hor += step; inc_depth += aim * step;}
+                        if words[0] == "down" { inc_aim += step; }
+                        if words[0] == "up" { inc_aim -= step; }
+                    }
+                    (hor + inc_hor, depth + inc_depth, aim + inc_aim) });
+
+            format!("{}", horizontal * depth)
         },
         _ => String::from(""),
     }
