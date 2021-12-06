76d45a2f4f6e5976017a7170d9aff3f387fdad1d
Richard Beneyton <richard.beneyton@gmail.com>
2021-12-07 00:19:34 +0100

    day 4, part 1

--- src/main.rs
+++ src/main.rs
@@ -186,6 +186,71 @@ fn solve(day: Day, part: u8, input: String) -> String {
 
             format!("{}", oxygen * co2)
         },
+        4 => if part == 1 {
+            let mut line = input.lines();
+            let numbers : Vec<_> = line.next().unwrap()
+                .split(',')
+                .map(|x| x.parse::<u8>().unwrap())
+                .collect();
+
+            const N : usize = 5;
+            type Board = [[(u8, bool); N]; N];
+            let mut boards = Vec::new();
+            loop {
+                match line.next() {
+                    None => break,
+                    Some(v) => if v.chars().count() > 0 { panic!("invalid parse pattern"); },
+                }
+                let mut board = Board::default();
+                for row in 0..N {
+                    let mut row_it = line.next().unwrap().split_whitespace();
+                    for col in 0..N {
+                        board[row][col].0 = row_it.next().unwrap().parse::<u8>().unwrap();
+                    }
+                }
+                boards.push(board);
+            }
+
+            for number in numbers {
+                for board in &mut boards {
+                    'place_num: for row in 0..N {
+                        for col in 0..N {
+                            if board[row][col].0 == number {
+                                board[row][col].1 = true;
+                                break 'place_num;
+                            }
+                        }
+                    }
+
+                    for scan in 0..N {
+                        if (board[scan][0].1
+                         && board[scan][1].1
+                         && board[scan][2].1
+                         && board[scan][3].1
+                         && board[scan][4].1)
+                         || (board[0][scan].1
+                          && board[1][scan].1
+                          && board[2][scan].1
+                          && board[3][scan].1
+                          && board[4][scan].1) {
+                            let mut sum = 0i64;
+                            for row in 0..N {
+                                for col in 0..N {
+                                    if board[row][col].1 == false {
+                                        sum += board[row][col].0 as i64;
+                                    }
+                                }
+                            }
+                            return format!("{}", sum * (number as i64));
+                        }
+                    }
+                }
+            }
+
+            String::from("")
+        } else {
+            String::from("")
+        },
         _ => String::from(""),
     }
 }
