f6836f1ef2ee075727e946c3ce249029718e8c41
Richard Beneyton <richard.beneyton@gmail.com>
2021-12-07 20:08:18 +0100

    day 6, part 1

--- src/main.rs
+++ src/main.rs
@@ -438,6 +438,38 @@ fn solve(day: Day, part: u8, input: String) -> String {
 
             format!("{}", sum_overlap)
         },
+        6 => if part == 1 {
+            const N : usize = 9;
+            type Fishs = [usize; N]; // 0..=8
+
+            let mut fishs = Fishs::default();
+            for line in input.lines() {
+                for tok in line.split(',').map(|x| x.parse::<usize>().unwrap()) {
+                    assert!(tok < N);
+                    fishs[tok] += 1;
+                }
+            }
+
+            for _day in 0..80 {
+                let mut new_fishs = Fishs::default();
+
+                for age in 0..N {
+                    if age == 0 {
+                        new_fishs[6] = fishs[age]; // return to age 6
+                        new_fishs[8] = fishs[age]; // spawn babies
+                    } else {
+                        new_fishs[age - 1] += fishs[age]; // decrease age counter
+                    }
+                }
+                fishs = new_fishs;
+            }
+
+            let sum : usize = fishs.iter().sum();
+
+            format!("{}", sum)
+        } else {
+            String::from("")
+        },
         _ => String::from(""),
     }
 }
