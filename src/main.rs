183e61a4d4f969513e8f7e6273094747291a6e34
Richard Beneyton <richard.beneyton@gmail.com>
2021-12-11 15:15:37 +0100

    day 11, part 2

--- src/main.rs
+++ src/main.rs
@@ -962,7 +962,71 @@ fn solve(day: Day, part: u8, input: String) -> String {
 
             format!("{}", flashes)
         } else {
-            String::from("")
+            let w = input.lines().next().unwrap().chars().count();
+            let h = input.lines().count();
+            assert_eq!(w, h);
+            let n = w;
+            let n_isize = n as isize;
+            let sz = n * n;
+
+            let mut grid = Vec::new();
+            grid.resize(sz, 0i8);
+            let idx_of = |x, y| x + n * y;
+
+            let mut idx = 0;
+            for line in input.lines() {
+                for c in line.chars() {
+                    grid[idx] = c.to_digit(10).unwrap() as i8;
+                    idx += 1;
+                }
+            }
+            assert_eq!(idx, sz);
+
+            let mut step = 0;
+            loop {
+                for idx in 0..sz {
+                    grid[idx] += 1;
+                }
+
+                let mut flashes = 0usize;
+                loop {
+                    let flashes_before = flashes;
+                    for row in 0..n {
+                        for col in 0..n {
+                            let v = grid[idx_of(col, row)];
+                            if v > 9 {
+                                flashes += 1;
+                                grid[idx_of(col, row)] = -1; // off
+                                // flash, so increase neighbors
+                                let (row, col) = (row as isize, col as isize);
+                                for arow in (row - 1)..=(row + 1) {
+                                    if arow < 0 || arow >= n_isize { continue; }
+                                    for acol in (col - 1)..=(col + 1) {
+                                        if acol < 0 || acol >= n_isize { continue; }
+                                        if grid[idx_of(acol as usize, arow as usize)] >= 0 {
+                                            grid[idx_of(acol as usize, arow as usize)] += 1;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if flashes_before == flashes {
+                        break;
+                    }
+                }
+                for idx in 0..sz {
+                    if grid[idx] < 0 {
+                        grid[idx] = 0;
+                    }
+                }
+                step += 1;
+                if flashes == sz {
+                    break;
+                }
+            }
+
+            format!("{}", step)
         }
         _ => String::from(""),
     }
