5810559591df4d40b1eaebb807ba61bb9142bce7
Richard Beneyton <richard.beneyton@gmail.com>
2021-12-12 15:47:02 +0100

    day 12, part 2: more efficient code
    before:450ms, afert:10ms, using trivial bitfields.

--- src/main.rs
+++ src/main.rs
@@ -1083,40 +1083,83 @@ fn solve(day: Day, part: u8, input: &String) -> String {
                 let entry = nodes.entry(to).or_insert(HashSet::new());
                 entry.insert(from);
             }
+            let nodes = nodes;
 
             fn upper<'a>(node : &'a str) -> bool { node.chars().all(char::is_uppercase) }
-            fn count_dup_small<'a>(path: &Vec<&'a str>) -> usize {
-                let mut path = path.iter()
-                    .filter(|x| x.chars().all(char::is_lowercase))
-                    .collect::<Vec<_>>();
-                path.sort();
-                path.iter()
-                    .tuple_windows()
-                    .filter(|(i, next)| i == next)
-                    .count()
+
+            let n = nodes.len();
+            // technically only lower nodes indices + one extra flag are required, so 63 lower
+            // nodes can be used using u64 (or use u128 with 127 lower nodes).
+            // we code naive method here: all indices (lower + upper) + extra indices, so only 32
+            assert!(n < 32);
+            let idx_of = |needle : &str| {
+                nodes.iter()
+                    .enumerate()
+                    .find_map(|(i, (f, _))| if *f == needle { Some(i) } else { None })
+                    .unwrap()
+            };
+
+            type Path = usize;
+            let mut nnodes = Vec::with_capacity(n);
+            nnodes.resize(n, Path::default());
+            let mut upper_mask = Path::default();
+            let mut lower_mask = Path::default();
+            for (idx, (from, tos)) in nodes.iter().enumerate() {
+                if upper(from) {
+                    upper_mask |= 1 << idx;
+                    upper_mask |= 1 << (idx + 32);
+                } else {
+                    lower_mask |= 1 << idx;
+                    lower_mask |= 1 << (idx + 32);
+                }
+                for to in tos {
+                    nnodes[idx] |= 1 << idx_of(to);
+                }
             }
-            fn visit<'a>(nodes: &HashMap<&'a str, HashSet<&'a str>>,
-                         from: &'a str,
-                         path: &Vec<&'a str>,
-                         paths: &mut Vec<Vec<&'a str>>)
+
+            fn count_dup_small(path: Path, lower_mask: usize) -> u32 {
+                ((path & lower_mask) & 0xFFFFFFFF00000000).count_ones()
+            }
+            fn visit(nnodes: &Vec<Path>,
+                     from_idx: usize,
+                     start_idx: usize,
+                     end_idx: usize,
+                     upper_mask: usize,
+                     lower_mask: usize,
+                     path: Path,
+                     paths: &mut usize)
             {
-                for next in nodes.get(from).unwrap() {
-                    if *next == "start" { continue; }
-                    let mut new = path.clone();
-                    new.push(next);
-                    if !upper(next) && count_dup_small(&new) > 1 { continue; }
-                    if *next == "end" {
-                        paths.push(new);
-                    } else {
-                        visit(nodes, next, &new, paths);
+                for next_idx in 0..32 {
+                    let next_flag = 1 << next_idx;
+                    if nnodes[from_idx] & next_flag != 0 {
+                        if next_idx == start_idx { continue; }
+                        let mut new_path = path;
+                        if next_flag & upper_mask == 0 {
+                            if new_path & next_flag != 0 {
+                                if new_path & next_flag << 32 != 0 { continue; }
+                                new_path |= next_flag << 32;
+                            } else {
+                                new_path |= next_flag;
+                            }
+                            if count_dup_small(new_path, lower_mask) > 1 { continue; }
+                        } else {
+                            new_path |= next_flag;
+                        }
+                        if next_idx == end_idx {
+                            *paths += 1;
+                        } else {
+                            visit(nnodes, next_idx, start_idx, end_idx, upper_mask, lower_mask, new_path, paths);
+                        }
                     }
                 }
             }
 
-            let mut paths = Vec::new();
-            visit(&nodes, "start", &vec!["start",], &mut paths);
+            let start_idx = idx_of("start");
+            let end_idx = idx_of("end");
+            let mut paths = 0;
+            visit(&nnodes, start_idx, start_idx, end_idx, upper_mask, lower_mask, 1 << start_idx, &mut paths);
 
-            format!("{}", paths.len())
+            format!("{}", paths)
         }
         _ => String::from(""),
     }
