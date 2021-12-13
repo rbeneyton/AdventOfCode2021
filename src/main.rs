10ef0e9b818a52a010e58a5666c7e2459f174f69
Richard Beneyton <richard.beneyton@gmail.com>
2021-12-13 12:24:53 +0100

    day 13, part 2

--- src/main.rs
+++ src/main.rs
@@ -1227,7 +1227,73 @@ fn solve(day: Day, part: u8, input: &String) -> String {
 
             format!("{}", visible_dots)
         } else {
-            String::from("")
+            let points = input.lines()
+                .filter_map(|x| if x.contains(',') {
+                    Some(x.split(',')
+                        .map(|x| x.parse::<usize>().unwrap())
+                        .collect_tuple::<(_, _)>()
+                        .unwrap())
+                } else { None })
+                .collect::<Vec<_>>();
+            let (w, h) = points.iter()
+                .fold((0, 0), |(w, h), (col, row)| (cmp::max(w, *col), cmp::max(h, *row)));
+            let (w, h) = (w + 1, h + 1);
+
+            let mut grid = Vec::new();
+            grid.resize(w * h, 0u8);
+            let idx_of = |x, y| x + w * y;
+            for (col, row) in points {
+                grid[idx_of(col, row)] = 1;
+            }
+
+            let (mut visible_w, mut visible_h) = (w, h);
+            for (what, off) in input.lines()
+                .filter_map(|x| if x.starts_with("fold along ") {
+                    let what = x.chars().skip(11)
+                        .next().unwrap();
+                    let off = x.split('=').skip(1).map(|x| x.parse::<usize>().unwrap())
+                        .next().unwrap();
+                    Some((what, off))
+                } else { None })
+            {
+                match what {
+                    'x' => {
+                        for row in 0..visible_h {
+                            for col in (off + 1)..visible_w {
+                                let proj = off - (col - off);
+                                grid[idx_of(proj, row)] += grid[idx_of(col, row)];
+                                if proj == 0 { break; }
+                            }
+                        }
+                        visible_w = off;
+                    },
+                    'y' => {
+                        for row in (off + 1)..visible_h {
+                            let proj = off - (row - off);
+                            for col in 0..visible_w {
+                                grid[idx_of(col, proj)] += grid[idx_of(col, row)];
+                            }
+                            if proj == 0 { break; }
+                        }
+                        visible_h = off;
+                    },
+                    _ => panic!("invalid axis"),
+                }
+            }
+
+            let mut res = String::from("\n");
+            for row in 0..visible_h {
+                for col in 0..visible_w {
+                    if grid[idx_of(col, row)] > 0 {
+                        res.push('X');
+                    } else {
+                        res.push(' ');
+                    }
+                }
+                res.push('\n');
+            }
+
+            res
         }
         _ => String::from(""),
     }
