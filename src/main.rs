55282c4ccc9c607061a847ed353192fbbb184086
Richard Beneyton <richard.beneyton@gmail.com>
2021-12-27 22:54:56 +0100

    day 17, part 2 (correct inputs)

--- src/main.rs
+++ src/main.rs
@@ -1862,37 +1862,37 @@ fn solve(day: Day, part: u8, input: &String) -> String {
             let input = input.replace("..", ",");
             let input = input.replace(", y=", ",");
             let (x1, x2, y1, y2) = input.split(",")
-                .map(|x| x.parse::<i64>().unwrap())
+                .map(|x| x.parse::<i32>().unwrap())
                 .collect_tuple()
                 .unwrap();
             println!("{}/{} {}/{}", x1, x2, y1, y2);
 
 
-            let launch = |x, y| {
-                let (mut x, mut y) = (x, y);
-                let mut pos = (0, 0);
+            let launch = |mut vx, mut vy| {
+                let (mut x, mut y) = (0, 0);
                 loop {
-                    pos.0 += x;
-                    pos.1 += y;
+                    x += vx;
+                    y += vy;
 
-                    if pos.0 >= x1 && pos.0 <= x2
-                    && pos.1 >= y1 && pos.1 <= y2 {
-                        break true;
+                    if vx > 0 { vx -= 1; }
+                    if vx < 0 { vx += 1; }
+                    vy -= 1;
+
+                    if x >= x1 && x <= x2
+                        && y >= y1 && y <= y2 {
+                            break 1;
                     }
-                    if pos.0 > x2 || pos.1 < cmp::min(0, y2) { break false; }
-
-                    if x > 0 { x -= 1; }
-                    if x < 0 { x += 1; }
-                    y -= 1;
-
-                    if x == 0 && pos.0 < x1 { break false; } // vertical fall
+                    if (x < x1 && vx == 0)
+                        || (y < y1 && vy < 0) {
+                            break 0;
+                    }
                 }
             };
 
             let mut n = 0;
-            for x in 1..=x2 {
-                for y in y2..100000 {
-                    if launch(x, y) { n += 1; }
+            for x in 0..=x2 {
+                for y in y1..10000 {
+                    n += launch(x, y);
                 }
             }
 
